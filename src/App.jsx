c33b11b6d829e98f140704033d556c51ab1c6f36
Lxng-visual <lovestephen168@gmail.com>
2026-01-21 19:39:39 +0700

    fix: set up the routes correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import AppLayout from "./apps/AppLayout"
-import { Routes, Route } from "react-router-dom"
-import { Bacii, MyLibrary, Home, ContactUs, Settings } from "./pages/index"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom"
+import { Login, Register, NotFound, Bacii, Home, ContactUs, MyLibrary, Settings } from "./pages"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />
+}
 
 function App() {
   return (
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/my-library" element={<MyLibrary/>}/>
-        <Route path="/bacii" element={<Bacii/>}/>
-        <Route path="/contact-us" element={<ContactUs/>}/>
-        <Route path="/settings" element={<Settings/>}/>
-      </Routes>
-    </AppLayout>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<RegisterAndLogout />} />
+      <Route path="/logout" element={<Logout />} />
+      
+      {/* Protected Routes with Layout */}
+      <Route
+        element={
+          <ProtectedRoute>
+            <AppLayout>
+              <Outlet />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/" element={<Home />} />
+        <Route path="/my-library" element={<MyLibrary />} />
+        <Route path="/bacii" element={<Bacii />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
 
+      {/* Catch all */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
